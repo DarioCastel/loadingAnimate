2f09d87a865998c4782f8fdb84d9c2d7cbd43b87
DarioCastel <lecastelnovo@gmail.com>
2022-12-12 19:13:59 -0300

    loading

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="loader">
+      <div className="loader-item"></div>
+      <div className="loader-item"></div>
+      <div className="loader-item"></div>
+    </div>
+  )
+}
+
+export default App
